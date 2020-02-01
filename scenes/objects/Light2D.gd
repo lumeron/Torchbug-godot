9842983f526ef6e5a9adfd1abe77515d1be638ec
lumeron <56777654+lumeron@users.noreply.github.com>
2020-02-01 13:20:21 +1100

    adjusted lights

---
+++ scenes/objects/Light2D.gd
@@ -0,0 +1,26 @@
+extends Light2D
+
+#var flick = 0
+## Declare member variables here. Examples:
+## var a = 2
+## var b = "text"
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	randomize()
+#
+#func _physics_process(delta):
+#	get_flicker()
+#	if (flick > 3):
+#		flicker()
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	pass
+#
+#func get_flicker():
+#	flick = randi()%5
+#
+#func flicker():
+#	self.energy = 0.4
+#	self.energy = 0.7
